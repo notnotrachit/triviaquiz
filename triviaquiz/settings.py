5cf745a9449886e148fa8f1397b4874d100a32d8
Rachit Khurana <rachitkhurana40@gmail.com>
2021-09-13 10:56:00 +0530

    Update settings.py

--- triviaquiz/settings.py
+++ triviaquiz/settings.py
@@ -141,7 +141,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # Default primary key field type
